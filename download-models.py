52c8f774327da7ff8336b420e7e84d5950cd7f69
Haochen Sun <haochen.sun@uwaterloo.ca>
2023-12-17 21:49:36 -0500

    narval

--- download-models.py
+++ download-models.py
@@ -4,5 +4,8 @@ import os
 os.environ['TRANSFORMERS_CACHE']="./model-storage"
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", token = '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')
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", token = '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')
+# tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", token = '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')
+# model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", token = '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')
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-13b-hf", token = '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')
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-13b-hf", token = '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')
--- quantize.sh
+++ quantize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --gpus=v100l:1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=48000M
+#SBATCH --gpus=a100:1
+#SBATCH --cpus-per-task=12 
+#SBATCH --mem=127500M
 #SBATCH --time=0-02:59
 #SBATCH --output=%N-%j.out
 #SBATCH --account=rrg-hongyanz
@@ -14,14 +14,14 @@ module load gcc/9.3.0 python/3.10 arrow/11.0.0 nodejs cuda/11.7 cmake protobuf c
 source $HOME/Quantization_ENV/bin/activate
 echo "The Python used is $(which python)"
 
-TRANSFORMERS_CACHE=./model-storage CUDA_VISIBLE_DEVICES=0 python llama.py meta-llama/Llama-2-7b-hf c4 --wbits 4 --true-sequential --act-order --groupsize 128 --save llama7b-4bit-128g.pt
+TRANSFORMERS_CACHE=./model-storage CUDA_VISIBLE_DEVICES=0 python llama.py meta-llama/Llama-2-7b-hf c4 --wbits 4 --true-sequential --act-order --groupsize 128 --eval
 # Benchmark generating a 2048 token sequence with the saved model
-TRANSFORMERS_CACHE=./model-storage CUDA_VISIBLE_DEVICES=0 python llama.py meta-llama/Llama-2-7b-hf c4 --wbits 4 --groupsize 128 --load llama7b-4bit-128g.pt --benchmark 2048 --check
+# TRANSFORMERS_CACHE=./model-storage CUDA_VISIBLE_DEVICES=0 python llama.py meta-llama/Llama-2-7b-hf c4 --wbits 4 --groupsize 128 --load llama7b-4bit-128g.pt --benchmark 2048 --check
 
-TRANSFORMERS_CACHE=./model-storage CUDA_VISIBLE_DEVICES=0 python llama.py meta-llama/Llama-2-7b-hf c4 --wbits 2 --true-sequential --act-order --groupsize 128 --save llama7b-2bit-128g.pt
+TRANSFORMERS_CACHE=./model-storage CUDA_VISIBLE_DEVICES=0 python llama.py meta-llama/Llama-2-7b-hf c4 --wbits 2 --true-sequential --act-order --groupsize 128 --eval
 # Benchmark generating a 2048 token sequence with the saved model
-TRANSFORMERS_CACHE=./model-storage CUDA_VISIBLE_DEVICES=0 python llama.py meta-llama/Llama-2-7b-hf c4 --wbits 2 --groupsize 128 --load llama7b-2bit-128g.pt --benchmark 2048 --check
+# TRANSFORMERS_CACHE=./model-storage CUDA_VISIBLE_DEVICES=0 python llama.py meta-llama/Llama-2-7b-hf c4 --wbits 2 --groupsize 128 --load llama7b-2bit-128g.pt --benchmark 2048 --check
 
-TRANSFORMERS_CACHE=./model-storage CUDA_VISIBLE_DEVICES=0 python llama.py meta-llama/Llama-2-7b-hf c4 --wbits 1 --true-sequential --act-order --groupsize 128 --save llama7b-1bit-128g.pt
+TRANSFORMERS_CACHE=./model-storage CUDA_VISIBLE_DEVICES=0 python llama.py meta-llama/Llama-2-7b-hf c4 --wbits 1 --true-sequential --act-order --groupsize 128 --eval
 # Benchmark generating a 2048 token sequence with the saved model
-TRANSFORMERS_CACHE=./model-storage CUDA_VISIBLE_DEVICES=0 python llama.py meta-llama/Llama-2-7b-hf c4 --wbits 1 --groupsize 128 --load llama7b-1bit-128g.pt --benchmark 2048 --check
+# TRANSFORMERS_CACHE=./model-storage CUDA_VISIBLE_DEVICES=0 python llama.py meta-llama/Llama-2-7b-hf c4 --wbits 1 --groupsize 128 --load llama7b-1bit-128g.pt --benchmark 2048 --check
